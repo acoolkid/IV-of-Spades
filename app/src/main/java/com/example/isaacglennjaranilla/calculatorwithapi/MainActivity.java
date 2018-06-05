52c5b237b70fc9b793d1978166b6b08a1a9af64b
Isaac Jaranilla <isaacglenn.jaranilla@yahoo.com>
2018-06-05 15:46:51 +0300

    pushing updates

---
+++ app/src/main/java/com/example/isaacglennjaranilla/calculatorwithapi/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.isaacglennjaranilla.calculatorwithapi;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    Intent i;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void doSomething (View v){
+
+        switch (v.getId()){
+            case R.id.btnCalculator:
+                i = new Intent(this,Calculator.class);
+                startActivity(i);
+                break;
+        }
+
+    }
+}
+
